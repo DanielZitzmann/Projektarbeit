30d2b2bed4f7a23a82d8c4cd292d66ba5c83a2b7
Daniel Zitzmann <daniel.zitzmann@mailbox.org>
2021-09-26 11:31:46 +0200

    body-parser entfernt, dotenv installiert

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const port = 3001;
+
+require("dotenv").config();
+app.use(express.json());
+
+//import routes
 var artikelroute = require("./routes/artikel.js");
 
-app.use(bodyParser.json());
-
+//middleware
 app.use("/artikel", artikelroute);
+
+console.log(process.env.SECRET_PW);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
