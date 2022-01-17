433dbf56d20df0f0127d98db29d9ab0dbeb2591d
Daniel Zitzmann <daniel.zitzmann@mailbox.org>
2022-01-17 12:02:16 +0100

    package.json ergänzt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
 import TagsContainer from "./Components/TagsContainer";
 import ArtikelContainer from "./Components/ArtikelContainer";
 import ListenContainer from "./Components/ListenContainer";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./Components/Navbar/NavbarComp";
-
 
 function App() {
     //TODO: React-Routes Navbar für Container-Komponenten nicht alles auf einmal anzeigen, auswählen über navbar
     return (
         <div>
-            <NavbarComp/>
-            <TagsContainer />
-            <ArtikelContainer />
-            <ListenContainer />
+            <NavbarComp />
         </div>
     );
 }
